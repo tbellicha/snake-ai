109d8f0fe02a532a4da8d168ea22f7f03650e6f7
tbellicha <tanguy.bellicha@epitech.eu>
2026-08-26 09:41:35 +0200

    Refactor SnakeGameAI to simplify penalty system and remove loop debt
    - Removed the `loop_debt` variable and its associated logic from the scoring system.
    - Streamlined the penalty calculation for revisiting positions, enhancing code clarity and maintainability.

--- snake_ai/game.py
+++ snake_ai/game.py
@@ -55,7 +55,6 @@ class SnakeGameAI:
         self._place_food()
         self.frame_iteration = 0
         self.visited_since_food = set()
-        self.loop_debt = 0.0
 
     def _place_food(self):
         x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
@@ -88,16 +87,10 @@ class SnakeGameAI:
             reward = 10
             self._place_food()
             self.visited_since_food = set()
-            self.loop_debt = 0.0
         else:
             self.snake.pop()
             if self.head in self.visited_since_food:
-                penalty = 1.0 / len(self.snake)
-                reward -= penalty
-                self.loop_debt += penalty
-                while self.loop_debt >= 1 and self.score > 0:
-                    self.score -= 1
-                    self.loop_debt -= 1
+                reward -= 1.0 / len(self.snake)
             else:
                 self.visited_since_food.add(self.head)
 
